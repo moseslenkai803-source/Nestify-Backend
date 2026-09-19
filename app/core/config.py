739bd2125f222d589694b697332b4cffd4cad581
Moses lenkai <moseslenkai803@gmail.com>
2026-09-19 15:08:47 +0300

    Configure SQLAlchemy PostgreSQL connection

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     app_version: str = "0.1.0"
     environment: str = "development"
     debug: bool = True
+    database_url: str
+
 
     model_config = SettingsConfigDict(
         env_file=".env",
