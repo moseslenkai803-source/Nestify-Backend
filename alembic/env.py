0804fa871be746d617d343239dba10a55788dba4
Moses lenkai <moseslenkai803@gmail.com>
2026-09-22 10:18:01 +0300

    Fix Alembic model metadata imports

--- alembic/env.py
+++ alembic/env.py
@@ -13,6 +13,7 @@ from app.models.property import Property
 from app.models.property_address import PropertyAddress
 from app.models.address_plate import AddressPlate
 from app.models.property_verification import PropertyVerification
+from app.models.property_access import PropertyAccess
 
 # this is the Alembic Config object, which provides
 
